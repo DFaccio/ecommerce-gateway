4904c64e6b0eb126e24f5245a9fa4235ee9ede5c
Isabela França <fysabelah@gmail.com>
2024-07-20 10:44:04 -0300

    Adicionar configuração para o Swagger do pagamento e carrinho

--- src/main/java/com/ecommerce_gateway/config/RoutesConfig.java
+++ src/main/java/com/ecommerce_gateway/config/RoutesConfig.java
@@ -1,11 +1,13 @@
 package com.ecommerce_gateway.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.route.Route;
 import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.Buildable;
+import org.springframework.cloud.gateway.route.builder.PredicateSpec;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 
 @Configuration
 public class RoutesConfig {
@@ -25,42 +27,38 @@ public class RoutesConfig {
     @Bean
     public RouteLocator custom(RouteLocatorBuilder routeLocatorBuilder) {
         return routeLocatorBuilder.routes()
-                .route("inventory", r -> r.path("/ecommerce/inventory/**")
-                        .and().not(p -> p.path("/ecommerce/inventory/documentation/**", "/ecommerce/inventory/doc/**"))
-                        .filters(f -> f.stripPrefix(2))
-                        .uri(productUri))
-                .route("payment", r -> r.path("/ecommerce/payment/**")
-                        .filters(f -> f.stripPrefix(2))
-                        .uri(paymentUri))
-                .route("shopping-cart", r -> r.path("/ecommerce/shopping-cart/**")
-                        .filters(f -> f.stripPrefix(2))
-                        .uri(shoppingCartUri))
-                .route("users", r -> r.path("/ecommerce/authentication-api/**")
-                        .and().not(p -> p.path("/ecommerce/authentication-api/documentation/**", "/ecommerce/authentication-api/doc/**"))
-                        .filters(f -> f.stripPrefix(2))
-                        .uri(userUri))
+                .route("inventory", r -> createRoute(r, "/ecommerce/inventory", productUri))
+                .route("payment", r -> createRoute(r, "/ecommerce/payment", paymentUri))
+                .route("shopping-cart", r -> createRoute(r, "/ecommerce/shopping-cart", shoppingCartUri))
+                .route("users", r -> createRoute(r, "/ecommerce/authentication-api", userUri))
                 .build();
+    }
+
+    private Buildable<Route> createRoute(PredicateSpec route, String path, String uri) {
+        return route.path(path + "/**")
+                .and().not(p -> p.path(path + "/documentation/**", path + "/doc/**"))
+                .filters(f -> f.stripPrefix(2))
+                .uri(uri);
     }
 
     @Bean
     public RouteLocator docs(RouteLocatorBuilder routeLocatorBuilder) {
         return routeLocatorBuilder.routes()
-                .route("inventory-doc-swagger-ui", r -> r.path("/ecommerce/inventory/doc/**")
-                        .filters(f -> f.stripPrefix(2)
-                                .rewritePath("/doc(?<segment>.*)", "/doc${segment}"))
-                        .uri(productUri))
-                .route("inventory-doc-swagger-json", r -> r.path("/ecommerce/inventory/documentation/**")
-                        .filters(f -> f.stripPrefix(2)
-                                .rewritePath("/documentation(?<segment>.*)", "/documentation${segment}"))
-                        .uri(productUri))
-                .route("authentication-doc-swagger-json", r -> r.path("/ecommerce/authentication-api/documentation/**")
-                        .filters(f -> f.stripPrefix(2)
-                                .rewritePath("/documentation(?<segment>.*)", "/documentation${segment}"))
-                        .uri(userUri))
-                .route("authentication-doc-swagger-ui", r -> r.path("/ecommerce/authentication-api/doc/**")
-                        .filters(f -> f.stripPrefix(2)
-                                .rewritePath("/doc(?<segment>.*)", "/doc${segment}"))
-                        .uri(userUri))
+                .route("inventory-doc-swagger-ui", r -> createDocumentationRoute(r, "/ecommerce/inventory/doc/**", "/doc", productUri))
+                .route("inventory-doc-swagger-json", r -> createDocumentationRoute(r, "/ecommerce/inventory/documentation/**", "/documentation", productUri))
+                .route("authentication-doc-swagger-json", r -> createDocumentationRoute(r, "/ecommerce/authentication-api/documentation/**", "/documentation", userUri))
+                .route("authentication-doc-swagger-ui", r -> createDocumentationRoute(r, "/ecommerce/authentication-api/doc/**", "/doc", userUri))
+                .route("payment-doc-swagger-json", r -> createDocumentationRoute(r, "/ecommerce/payment/documentation/**", "/documentation", paymentUri))
+                .route("payment-doc-swagger-ui", r -> createDocumentationRoute(r, "/ecommerce/payment/doc/**", "/doc", paymentUri))
+                .route("cart-doc-swagger-json", r -> createDocumentationRoute(r, "/ecommerce/shopping-cart/documentation/**", "/documentation", shoppingCartUri))
+                .route("cart-doc-swagger-ui", r -> createDocumentationRoute(r, "/ecommerce/shopping-cart/doc/**", "/doc", shoppingCartUri))
                 .build();
+    }
+
+    private Buildable<Route> createDocumentationRoute(PredicateSpec route, String path, String documentationPrefix, String uri) {
+        return route.path(path)
+                .filters(f -> f.stripPrefix(2)
+                        .rewritePath(documentationPrefix + "(?<segment>.*)", documentationPrefix + "${segment}"))
+                .uri(uri);
     }
 }
